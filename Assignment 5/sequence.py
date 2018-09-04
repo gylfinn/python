8bb8b8c6bc6e13273c93ce5dfa6dc88a68f52800
Gylfi Helgason <gylfisuf@gmail.com>
2018-09-04 14:35:40 +0000

    sequence.py hugsun

--- Assignment 5/sequence.py
+++ Assignment 5/sequence.py
@@ -1,1 +1,22 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+teljari = 2
+
+num1 = 1
+num2 = 2
+num3 = 3
+num3temp = 0
+num2temp = 0
+num1temp = 0
+
+while n > teljari:
+    num3temp = num3
+    num2temp = num2
+    num1temp = num1
+    num3 = num1 + num2 + num3
+    num2 = num3temp
+    num1 = num2temp
+
+    print(num3)
+    teljari += 1
+
