7e2e5cfec01ed6a120b5d83aa2983f8077bade9b
Gylfi Helgason <gylfisuf@gmail.com>
2018-09-04 14:58:29 +0000

    sequence.py gert flottara og betra

--- Assignment 5/sequence.py
+++ Assignment 5/sequence.py
@@ -1,24 +1,24 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
 
-teljari = 3
+teljari = 0
 
 num1 = 1
 num2 = 2
 num3 = 3
-num3temp = 0
+
 num2temp = 0
-num1temp = 0
-print(num1)
-print(num2)
-print(num3)
+num3temp = 0
+
 while n > teljari:
+
+    print(num1)
+
     num3temp = num3
     num2temp = num2
-    num1temp = num1
+    
     num3 = num1 + num2 + num3
     num2 = num3temp
     num1 = num2temp
 
-    print(num3)
     teljari += 1
 
